44aa3df52b09966b3b0221b6f8441a516823bdeb
Ayesoro Boluwatife <abraham@Ayesoros-MacBook-Pro.local>
2025-04-05 18:53:35 +0100

    test: trial fix for render error

--- Backend/server.js
+++ Backend/server.js
@@ -16,13 +16,13 @@ app.use(express.json());  //middleware that allows us to accept the JSON data in
 
 app.use("/api/products", productRoute);
 
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/Frontend/dist")));
+// if(process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 
-    app.get("*", (req,res) => {
-        res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
-    });
-}
+//     app.get("*", (req,res) => {
+//         res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
+//     });
+// }
 
 app.listen(PORT, () => {
 connectDB();
